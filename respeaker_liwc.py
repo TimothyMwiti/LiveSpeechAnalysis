2d6d1058ea017281ccc65fcabf73b63e4d5bca56
tiiltlab <36665023+tiiltlab@users.noreply.github.com>
2018-03-06 23:58:56 -0600

    Added files for prosodic and liwc analysis

---
+++ respeaker_liwc.py
@@ -0,0 +1,93 @@
+import nltk
+from nltk.stem import PorterStemmer
+from nltk import wordpunct_tokenize
+
+def listContains(l, word):
+	for item in l:
+		if item == word:
+			return 1
+	return 0
+
+def listContainsVariant(l, word):
+	for item in l:
+		if item.endswith("*"):
+			nItem = item.replace('*','').strip()
+			if word.startswith(nItem):
+				return 1
+		else:
+			if item == word:
+				return 1
+	return 0
+def populate_dictionary_index(iq):	
+	print "populating dictionary"
+	s=0
+	listofEmots=[]
+	liwcDictionary={}
+	for line in iq:
+		words = line.strip().split(',')
+		if s > 0:
+			for item in range(0,len(words)):
+				if len(words) > item:
+					word = words[item].lower().strip()
+					if word!= "":
+						cList = []
+						if liwcDictionary.has_key(listofEmots[item]):
+							cList = liwcDictionary[listofEmots[item]]
+						if (listContains(cList,word)==0):
+							cList.append(word)
+							liwcDictionary[listofEmots[item]] = cList;
+		else:
+			emotWord = ''
+			for word in words:
+				if (word!=''):
+					emotWord = word
+				listofEmots.append(emotWord)		
+		s=s+1
+	listofEmots = list(set(listofEmots))
+	return listofEmots, liwcDictionary
+
+def write_to_file(output, listofEmots, wordCount, wordDictionary):
+	print "writing to file"
+	for word in listofEmots:
+		output.write(word + "\t")
+	output.write("\n")
+	for emot in listofEmots:
+		if wordDictionary.has_key(emot):
+			output.write(str(float(wordDictionary[emot])) + '\t')
+		else:
+			output.write("0.0\t")
+	output.write(str(wordCount)+'\n')
+
+def process_text(txt, liwcDictionary, listofEmots, stemmer=PorterStemmer()):
+	print "processing text"
+	wordDictionary = {}
+	strstopwords = [str(w).lower() for w in stopwords.words('english')]
+
+	c_text = nltk.wordpunct_tokenize(txt)
+	base_words = [word.lower() for word in c_text]
+	stemmed_words = [stemmer.stem(word.lower()) for word in base_words]
+	non_stop = [word.lower() for word in base_words if word.lower() not in strstopwords]
+	no_punct = [word.lower() for word in base_words if word.lower().isalpha()]
+	wordCount = 0
+	for cWord in no_punct:
+		cWord = cWord.lower().strip()
+		print cWord
+		for emot in listofEmots:
+			if emot!='Entry':
+				print "Emotion", emot
+				if (listContainsVariant(liwcDictionary[emot], cWord)==1):
+					emotCount = 0
+					if (wordDictionary.has_key(emot)):
+						emotCount = wordDictionary[emot]
+					emotCount = emotCount +1
+					wordDictionary[emot]=emotCount
+		wordCount = wordCount + 1
+	return wordCount, wordDictionary
+if __name__ == '__main__':
+	iq = open("LIWC2007dictionary poster marcelo.csv", 'r')
+	emots, liwcDictionary = populate_dictionary_index(iq)
+	for a in range(10):
+		r = raw_input("Input text\n")
+		count, emot_dict = process_text(r, liwcDictionary, emots)
+		print count, emot_dict
+	#output all of the counts to a file based on listofEmots
---
+++ respeaker_prosody.py
@@ -0,0 +1,100 @@
+import parselmouth, os
+import numpy as np
+from nltk import wordpunct_tokenize
+from scipy import stats
+#from nltk.utils import ngrams
+from copy import deepcopy
+
+def extract_prosodic_features(audio_source, slope_cutoff=0.500, end_cutoff=0.2):
+    s =parselmouth.Sound(audio_source)
+    p=s.to_pitch()
+    voiced_frames = {}
+    for i in range(p.get_number_of_frames()):
+        if str(p.get_value_in_frame(i))!='nan':
+            voiced_frames[p.get_time_from_frame_number(i)] = p.get_value_in_frame(i)
+    sorted_times = sorted(voiced_frames.keys())
+    fo_slope_cutoff = sorted_times[-1]-slope_cutoff
+    fo_end_cutoff = sorted_times[-1]-end_cutoff
+    fo_slope_vals = []
+    fo_end_vals = []
+    o_fos = []
+    for i in sorted(voiced_frames.keys()):
+        c_fo = voiced_frames[i]
+        if i >=fo_end_cutoff:
+            fo_end_vals.append(c_fo)
+        else:
+            o_fos.append(c_fo)
+        if i >=fo_slope_cutoff:
+            fo_slope_vals.append([i, c_fo])
+    fo_slope_array = np.array(fo_slope_vals)
+    slope, intercept, r_value, p_value, std_err = stats.linregress(fo_slope_array[:,0],fo_slope_array[:,1])
+    #fo_slope = np.gradient(np.array(fo_slope_vals), axis=0)
+    fo_slope = slope
+    return p, fo_slope_vals, fo_slope, fo_end_vals, o_fos
+    #return fo_slope
+    
+def get_contour_features(data):
+    rev_data = deepcopy(data)
+    rev_data.reverse()
+    raise_ = []
+    fall_ = []
+    for i in range(len(rev_data)-1):
+        data_diff = rev_data[i] - rev_data[i+1]
+        if data_diff >0:
+            raise_.append(data_diff)
+        else:
+            fall_.append(data_diff)
+    raise_sum = sum(raise_)
+    fall_sum = -1*sum(fall_)
+    raise_count = len(raise_)
+    fall_count = len(fall_)
+    return raise_count, raise_sum, fall_count, fall_sum, raise_sum > fall_sum
+
+def check_question_words(txt):
+    question_words = ["what","how","why","when", "where","who", "which","whose","whom"]
+    inquiry_words = ["do","can", "if","did", "will", "you","is"]
+    #ngram_words = ["do you", "what is", "can we"]
+    #txt_ngrams = ngrams(wordpunct_tokenize(txt),2)
+    quest_indices =[]
+    inquiry_indices = []
+    txt_l = txt.lower()
+    for word in question_words:
+        if word in txt_l:
+            quest_indices.append(txt_l.index(word))
+    for word in inquiry_words:
+        if word in txt_l:
+            inquiry_indices.append(txt_l.index(word))
+    return quest_indices, inquiry_indices
+
+def extract_general_stats(data):
+    mx = max(data)
+    mn = min(data)
+    rng = mx-mn
+    mean = np.mean(data)
+    std = np.std(data)
+    return mx, mn, rng, mean, std
+
+if __name__ == '__main__':
+	c_txt = "What is your idea"
+	#get pertinent question related features from the text
+	quest_indices, inquiry_indices = check_question_words(c_txt)
+	utterance_length = wordpunct_tokenize(c_txt)
+
+	#get the prosodic features information
+	p, fo_slope_vals, fo_slope, fo_end_vals, o_fos=extract_prosodic_features("0001_5.0_7.9_Female_2_plate_build_4.wav")
+
+	#also compute max, min, mean, median and range of fo_end_vals
+	fo_max, fo_min, fo_range, fo_mean, fo_stdev = extract_general_stats(fo_end_vals)
+
+	#also compute max, min, mean, median and range of o_fos
+	ofo_max, ofo_min, ofo_range, ofo_mean, ofo_stdev = extract_general_stats(o_fos)
+
+	#compute difference between final 200 ms and the rest
+	#np.mean(fo_end_vals) - np.mean(o_fos)
+	raise_count, raise_sum, fall_count, fall_sum, raise_sum_fall_sum = get_contour_features(fo_end_vals)
+
+	#Should take into account previous utterance and subsequent utterance
+
+	#set up basic rules based on the length of the utterance - also - look for "Yes","No"
+
+	#use this data to train a decision tree classifier and examine its accuracy
